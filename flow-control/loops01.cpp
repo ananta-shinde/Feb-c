1b971a880730fc4e29d5f184ae545e2f7173675a
aShinde <ananta.shinde@honeywellcom>
2024-02-27 10:18:28 +0530

    27/02/24

---
+++ flow-control/loops01.cpp
@@ -0,0 +1,20 @@
+// for loop : for(initialization;condition;inc/dec)
+//{
+
+//}
+
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int num;// declaration
+    int temp;// initialization
+    printf("enter number :");
+    scanf("%d",&num);
+    for(int i=0;i<=num;i++)
+    {
+        printf("%d\t",i);
+    }
+
+    
+    return 0;
+}
