b885e3b05abd7410cecdf7d646b714d06b0fb9e9
aShinde <ananta.shinde@honeywellcom>
2024-03-04 10:25:32 +0530

    04/03/24

---
+++ flow-control/pattern03.cpp
@@ -0,0 +1,23 @@
+//    *
+//   **
+//  ***
+// ****
+
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    
+    for(int j=0;j<4;j++){
+        for(int i=0;i<4;i++){
+            if(i>=(3-j))
+            {
+                printf("*");
+            }else{
+                printf(" ");
+            }
+            
+        }
+        printf("\n");
+    }
+    return 0;
+}
---
+++ flow-control/pattern04.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    for(int j=0;j<4;j++)
+    {
+        for(int i=0;i<4;i++)
+        {
+            if(i<=j)
+            {
+                printf("*");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
