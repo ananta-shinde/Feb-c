f73adb6f5c902e51936dd5218c71cf0fd6d175b6
aShinde <ananta.shinde@honeywellcom>
2024-03-12 09:55:14 +0530

    12/03/24

---
+++ flow-control/array01.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    int numbers[5];
+    numbers[0] = 200;
+    numbers[1] = 300;
+    numbers[2] = 250;
+    numbers[3] = 22;
+    numbers[4] = 220;
+
+    printf("%d",numbers[3]);
+    
+    return 0;
+}
